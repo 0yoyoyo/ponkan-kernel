13bca5d1aee75dee27f3f6250cdf955dbdcd8d0e
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2021-09-11 17:57:39 +0900

    day03c

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,21 @@ fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
 }
 
 #[no_mangle]
-extern "C" fn kernel_main() -> ! {
+#[allow(clippy::missing_safety_doc)]
+pub unsafe extern "C" fn kernel_main(
+    frame_buffer_base: u64,
+    frame_buffer_size: u64,
+) -> ! {
+    let frame_buffer_base =
+        core::mem::transmute::<_, *mut u8>(frame_buffer_base);
+    let frame_buffer_size = frame_buffer_size as usize;
+    let frame_buffer =
+        core::slice::from_raw_parts_mut(frame_buffer_base, frame_buffer_size);
+    for (i, frame) in frame_buffer.iter_mut().enumerate() {
+        *frame = (i % 256) as u8;
+    }
+
     loop {
-        unsafe {
-            asm!("hlt");
-        }
+        asm!("hlt");
     }
 }
